402284ce16939b7fa85f4851b05ebce5b295d91a
Xierra <xierra@example.com>
2026-06-13 18:08:37 +0800

    Replace Font Awesome CDN reference with local hosted assets

--- app/Modules/Login/Views/register.php
+++ app/Modules/Login/Views/register.php
@@ -16,7 +16,7 @@ $randomGame = $games[array_rand($games)];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register - Nexus Rental</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/vendor/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?= base_url('vendor/bootstrap/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/auth.css') ?>">
 </head>
